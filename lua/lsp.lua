547be15d23d839ce180fb685a9e59732b8477ab3
Kento Okura <kentookura@gmail.com>
2024-10-22 21:24:25 +0200

    don't format using ocamlls

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -70,7 +70,11 @@ local on_attach = function(bufnr)
 
 	-- Create a command `:Format` local to the LSP buffer
 	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-		vim.lsp.buf.format()
+		vim.lsp.buf.format({
+			filter = function(client)
+				return client.name ~= "ocamlls"
+			end,
+		})
 	end, { desc = "Format current buffer with LSP" })
 end
 
